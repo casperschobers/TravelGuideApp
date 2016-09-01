db52f5aaa401d04c8c056fb35d66be0fc7858c97
CasperSchobers <casperschobers@live.nl>
2016-09-01 13:36:44 +0200

    added android tabbar styling

--- TravelGuide/www/js/app.js
+++ TravelGuide/www/js/app.js
@@ -35,7 +35,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     .state('tab', {
     url: '/tab',
     abstract: true,
-    templateUrl: 'templates/tabs.html'
+    templateUrl: function() {
+      if (ionic.Platform.isAndroid()) {
+        return  "templates/tabs-android.html";
+      }
+      return "templates/tabs.html";
+    }
   })
 
   // Each tab has its own nav history stack:
